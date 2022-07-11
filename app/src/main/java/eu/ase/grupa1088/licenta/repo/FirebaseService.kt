f70edcdc764e4ef31cb3ee0bc04b923af05ea9d6
Marius Stroescu <stroescump@gmail.com>
2022-07-12 00:33:15 +0300

    prettying code

--- app/src/main/java/eu/ase/grupa1088/licenta/repo/FirebaseService.kt
+++ app/src/main/java/eu/ase/grupa1088/licenta/repo/FirebaseService.kt
@@ -13,12 +13,12 @@ import eu.ase.grupa1088.licenta.utils.getUser
 sealed class FirebaseEndpoints(val path: String) {
     object Users : FirebaseEndpoints("Users")
     object MedicalAppointments : FirebaseEndpoints("Appointments")
+    object Doctors : FirebaseEndpoints("Doctors")
 }
 
 fun getUserAccountDetails(completionHandler: (user: AppResult<User>) -> Unit) =
-    FirebaseDatabase.getInstance().reference.child(Users.path).child(
-        FirebaseAuth.getInstance().currentUser?.uid
-            ?: throw IllegalArgumentException("UID must be valid for user ${FirebaseAuth.getInstance().currentUser}")
+    getFirebaseRoot().child(Users.path).child(
+        getCurrentUserUID() ?: throwUIDException()
     ).get().addOnSuccessListener {
         it.getUser()?.let { user ->
             completionHandler(AppResult.Success(user))
@@ -29,30 +29,40 @@ fun getUserAccountDetails(completionHandler: (user: AppResult<User>) -> Unit) =
     }
 
 fun getUserAppointmentsFirebase(completionHandler: (appointments: AppResult<List<MedicalAppointment>>) -> Unit) =
-    FirebaseDatabase.getInstance().reference.child(MedicalAppointments.path).child(
-        FirebaseAuth.getInstance().currentUser?.uid
-            ?: throw IllegalArgumentException("UID must be valid for user ${FirebaseAuth.getInstance().currentUser}")
+    getFirebaseRoot().child(MedicalAppointments.path).child(
+        getCurrentUserUID() ?: throwUIDException()
     ).get().addOnSuccessListener { dataSnapshot ->
-        dataSnapshot.getAppointments()?.let { user ->
-            completionHandler(AppResult.Success(user.entries.map { appointment ->
-                appointment.value.copy(id = appointment.key)
-            }.toList()))
+        dataSnapshot.getAppointments()?.let { appointmentHashMap ->
+            completionHandler(AppResult.Success(prepareMedicalAppointments(appointmentHashMap)))
         }
             ?: completionHandler(AppResult.Error(IllegalArgumentException("No medical appointments for requested input. Please contact owner of the app.")))
     }.addOnFailureListener {
         completionHandler(AppResult.Error(it))
     }
 
+private fun prepareMedicalAppointments(user: HashMap<String, MedicalAppointment>) =
+    user.entries.map { appointment ->
+        appointment.value.copy(id = appointment.key)
+    }.toList()
+
 fun deleteAppointmentFirebase(
     appointmentId: String,
     pos: Int,
     completionHandler: (AppResult<Int>) -> Unit
 ) =
-    FirebaseDatabase.getInstance().reference.child(MedicalAppointments.path).child(
-        FirebaseAuth.getInstance().currentUser?.uid
-            ?: throw IllegalArgumentException("UID must be valid for user ${FirebaseAuth.getInstance().currentUser}")
+    getFirebaseRoot().child(MedicalAppointments.path).child(
+        getCurrentUserUID()
+            ?: throwUIDException()
     ).child(appointmentId).removeValue().addOnSuccessListener {
         completionHandler(AppResult.Success(pos))
     }.addOnFailureListener {
         completionHandler(AppResult.Error(it))
     }
+
+private fun throwUIDException(): Nothing =
+    throw IllegalArgumentException("UID must be valid for user ${FirebaseAuth.getInstance().currentUser}")
+
+
+private fun getFirebaseRoot() = FirebaseDatabase.getInstance().reference
+
+private fun getCurrentUserUID() = FirebaseAuth.getInstance().currentUser?.uid
