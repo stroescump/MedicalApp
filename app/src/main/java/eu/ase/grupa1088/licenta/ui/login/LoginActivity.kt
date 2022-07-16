f266e4492f4815f3c6d6e8cdf7b85b5e7b0bb88b
Marius Stroescu <stroescump@gmail.com>
2022-07-16 18:21:49 +0300

    disable autologin

--- app/src/main/java/eu/ase/grupa1088/licenta/ui/login/LoginActivity.kt
+++ app/src/main/java/eu/ase/grupa1088/licenta/ui/login/LoginActivity.kt
@@ -29,9 +29,9 @@ class LoginActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         FirebaseApp.initializeApp(this)
         super.onCreate(savedInstanceState)
-        binding.etPassword.text = "123456".toEditable()
-        binding.etEmail.text = "stroescump@gmail.com".toEditable()
-        binding.btnSignIn.performClick()
+//        binding.etPassword.text = "123456".toEditable()
+//        binding.etEmail.text = "thetests1234@gmail.com".toEditable()
+//        binding.btnSignIn.performClick()
     }
 
     override fun setupListeners() {
